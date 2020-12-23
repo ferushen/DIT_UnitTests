38f792bf42c389fbd0bbf20103cca07539eb5aab
Ferushen <fbaka@list.ru>
2020-12-23 08:41:00 +0300

    add unit tests

--- Examples/Testing/UnitTests/CalendarServiceTests.cs
+++ Examples/Testing/UnitTests/CalendarServiceTests.cs
@@ -42,5 +42,55 @@ namespace UnitTests
             // Assert
             result.Should().Be(new DateTime(2020, 10, 03));
         }
+        
+        [Test]
+        public void GetWorkingToday_AllDatesWorking_ReturnsToday()
+        {
+            // Arrange
+            var today = new DateTime(2020, 12, 07);
+            
+            var dayOfWeekService = new Mock<IDayOfWeekService>();
+            dayOfWeekService
+                .Setup(x => x.IsWeekend(It.IsAny<DateTime>()))
+                .Returns(false);
+
+            var service = new CalendarService(new DayShiftService(dayOfWeekService.Object));
+            
+            // Act
+            var result = service.GetWorkingToday(today);
+            
+            // Assert
+            result.Should().Be(new DateTime(2020, 12, 07));
+        }
+        
+        [Test]
+        public void GetWorkingYesterday_AllDatesWorking_ReturnsYesterday()
+        {
+            // Arrange
+            var today = new DateTime(2020, 12, 08);
+
+            var service = new CalendarService(new DayShiftService(new DayOfWeekService()));
+            
+            // Act
+            var result = service.GetWorkingYesterday(today);
+            
+            // Assert
+            result.Should().Be(new DateTime(2020, 12, 07));
+        }
+        
+        [Test]
+        public void GetWorkingYesterday_RealLogic_Monday_ReturnsFriday()
+        {
+            // Arrange
+            var today = new DateTime(2020, 12, 07);
+
+            var service = new CalendarService(new DayShiftService(new DayOfWeekService()));
+
+            // Act
+            var result = service.GetWorkingYesterday(today);
+
+            // Assert
+            result.Should().Be(new DateTime(2020, 12, 04));
+        }
     }
 }
