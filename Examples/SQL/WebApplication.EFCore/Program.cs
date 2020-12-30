717354c8656ebf97b4a75976164cfd6ea0ddba56
Ferushen <fbaka@list.ru>
2020-12-30 17:45:29 +0300

    lr_1

--- Examples/SQL/WebApplication.EFCore/Program.cs
+++ Examples/SQL/WebApplication.EFCore/Program.cs
@@ -12,6 +12,10 @@ namespace WebApp.EFCore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>(); 
+                    webBuilder.UseUrls("http://0.0.0.0:5000");
+                });
     }
 }
---
+++ Examples/books.dockerfile
@@ -0,0 +1,7 @@
+﻿FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
+COPY . /app
+WORKDIR /app 
+RUN dotnet restore
+RUN dotnet publish -c Release -o build "SQL/WebApplication.EFCore/WebApplication.EFCore.csproj"
+    EXPOSE 5000/tcp
+ENTRYPOINT ["dotnet", "build/WebApplication.EFCore.dll"]
